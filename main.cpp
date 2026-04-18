e00ac898c6a620dd211ea3fda56b6eea909f0874
rococorbalan <corbaroco@gmail.com>
2026-04-18 02:49:09 -0300

    Add memory, stack, timers and font.
    Allocate 4KB for the memory. Use a uint8 stack to
    represent the stack and uint8 for the timers.
    The font is saved as an array an added to the
    memory on construction of the machine, starting at
    0x50 address.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,84 @@
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <iostream>
+#include <stack>
+#include <chrono>
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_AudioStream *stream = NULL;
+static int current_sine_sample = 0;
+
+using namespace std;
+
+uint8_t font[] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+struct Machine {
+    uint8_t* memory = new uint8_t[0x1000]{}; // 4KB initialized to 0
+
+    stack<uint8_t> pila; // stack
+
+    uint8_t sound_timer = 0;
+    uint8_t delay_timer = 0;
+    uint64_t last_tick = 0;
+
+    Machine() {
+        // Set font in memory
+        memcpy(memory + 0x50, font, sizeof(font));
+    }
+};
+
+Machine m;
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+    SDL_AudioSpec spec;
+
     SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 64, 32, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_Log("%u", m.memory[0x9E]);
+
+    spec.channels = 1;
+    spec.format = SDL_AUDIO_F32;
+    spec.freq = 8000;
+    stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
+    if (!stream) {
+        SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    /* SDL_OpenAudioDeviceStream starts the device paused. You have to tell it to start! */
+    SDL_ResumeAudioStreamDevice(stream);
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
@@ -31,12 +89,50 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
+
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+        SDL_Log("beep");
+        m.sound_timer = 1;
+    }
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    uint64_t now2 = SDL_GetTicks(); // milliseconds since start
+
+    if (now2 - m.last_tick >= 1000 / 60) { // ~16ms per tick
+        if (m.delay_timer > 0) m.delay_timer--;
+        if (m.sound_timer > 0) m.sound_timer--;
+        SDL_Log("%u", m.sound_timer);
+        m.last_tick = now2;
+    }
+
+    const int minimum_audio = (8000 * sizeof (float)) / 2;  /* 8000 float samples per second. Half of that. */
+    if (SDL_GetAudioStreamQueued(stream) < minimum_audio) {
+        if (m.sound_timer > 0) {
+            static float samples[512];  /* this will feed 512 samples each frame until we get to our maximum. */
+            int i;
+
+            /* generate a 440Hz pure tone */
+            for (i = 0; i < SDL_arraysize(samples); i++) {
+                const int freq = 440;
+                const float phase = current_sine_sample * freq / 8000.0f;
+                samples[i] = SDL_sinf(phase * 2 * SDL_PI_F);
+                current_sine_sample++;
+            }
+
+            /* wrapping around to avoid floating-point errors */
+            current_sine_sample %= 8000;
+
+            /* feed the new data to the stream. It will queue at the end, and trickle out as the hardware needs more data. */
+            SDL_PutAudioStreamData(stream, samples, sizeof (samples));
+        } else {
+            SDL_FlushAudioStream(stream); // end any remaining audio
+        }
+    }
+
     const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
     /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
     const float red = (float) (0.5 + 0.5 * SDL_sin(now));
